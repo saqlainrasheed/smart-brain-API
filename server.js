87a6f6d031878ae4f466d50965153bc6321a1e1f
Saqlain-Rasheed <msaqi681@gmail.com>
2020-04-21 15:59:20 +0500

    enviroment variable set

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.put('/image',(req,res)=>{(req,res,db)=>Image.handleImage(req,res,db)});
 
 app.post('/imageurl',(req,res)=>Image.handleApiCall(req,res));
 
-app.listen(3000,()=>{
-  console.log('running on port 3000...');
+app.listen(process.env.PORT || 3000,()=>{
+  console.log(`app is running on port ${process.env.PORT}`);
 });
 
